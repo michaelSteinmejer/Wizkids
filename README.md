0b6ee204ed78786dd4e0d6d97c7f1e7b2f62af14
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2019-12-14 14:37:07 +0100

    publish
    publishing brach to pepare for test

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wizkids
+ Test
---
+++ Wizkids/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wizkids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
